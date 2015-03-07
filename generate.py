de6598f90f7f066a20093a235224bc72b5515ef7
Vitaly Pavlenko <cxielamiko@gmail.com>
2015-03-07 21:05:56 +0300

    prototype

--- generate.py
+++ generate.py
@@ -1,21 +1,22 @@
 # -*- coding: utf-8 -*-
 
+from pprint import pprint
+
 import openpyxl
 
 def load_logs(file_name):
     logs = []
     wb = openpyxl.load_workbook(file_name)
-    log_number = -1
     for ws in wb:
-        log_number += 1
-        logs.append({})
-        logs[log_number]['date'] = ws['A2'].value
-        logs[log_number]['arena'] = ws['B2'].value
-        logs[log_number]['city'] = ws['C2'].value
-        logs[log_number]['attendance'] = ws['D2'].value
-        logs[log_number]['home-team'] = ws['E2'].value
-        logs[log_number]['guest-team'] = ws['F2'].value
-        logs[log_number]['players'] = [{}, {}]
+        log = {}
+        logs.append(log)
+        log['date'] = ws['A2'].value
+        log['arena'] = ws['B2'].value
+        log['city'] = ws['C2'].value
+        log['attendance'] = ws['D2'].value
+        log['home-team'] = ws['E2'].value
+        log['guest-team'] = ws['F2'].value
+        log['players'] = [{}, {}]
         home_team_last_row = 4
         for i in range(4, 31):
             if ws['A'+str(i)].value == 'guest-team':
@@ -27,28 +28,75 @@ def load_logs(file_name):
                 guest_team_last_row = i - 2
                 break
         for i in range(5, home_team_last_row+1):
-            logs[log_number]['players'][0][ws['B'+str(i)].value] = [ws['C'+str(i)].value, ws['D'+str(i)].value]
+            log['players'][0][ws['B'+str(i)].value] = [ws['C'+str(i)].value, ws['D'+str(i)].value]
         for i in range(home_team_last_row+3, guest_team_last_row+1):
-            logs[log_number]['players'][1][ws['B'+str(i)].value] = [ws['C'+str(i)].value, ws['D'+str(i)].value]
+            log['players'][1][ws['B'+str(i)].value] = [ws['C'+str(i)].value, ws['D'+str(i)].value]
 
         row = ws.get_highest_row()
-        logs[log_number]['score'] = [ws['B'+str(row-1)].value, ws['B'+str(row)].value]
+        log['score'] = [ws['B'+str(row-1)].value, ws['B'+str(row)].value]
         
-        logs[log_number]['goals'] = []
+        log['goals'] = []
         for i in range(guest_team_last_row+3, row-2):
             if ws['J'+str(i)].value == 'scored':
-                logs[log_number]['goals'].append([ws['B'+str(i)].value, ws['C'+str(i)].value])
+                log['goals'].append([ws['B'+str(i)].value, ws['C'+str(i)].value])
     return logs
 
-def form_report(logs):
-    reports = []
-    for log in logs:
-        reports.append(log['home-team']+' '+log['guest-team'])
-    return reports
-    
+
+class Event:
+    def is_applicable(self):
+        return True
+
+    def wrap(self, message):
+        return message
+
+    def gen_russian(self):
+        raise NotImplementedError()
+
+    def gen_english(self):
+        raise NotImplementedError()
+
+
+class WinnerEvent(Event):
+    def __init__(self, log):
+        if log['score'][0] > log['score'][1]:
+            self.winner, self.looser = log['home-team'], log['guest-team']
+        else:
+            self.looser, self.winner = log['home-team'], log['guest-team']
+        self.score = '-'.join(map(str, sorted(log['score'], reverse=True)))
+
+    def wrap(self, message):
+        return '<prosody rate="slow" pitch="high">' + message + '</prosody>'
+
+    def gen_russian(self):
+        return self.wrap(self.winner + ' обыграл ' + self.looser + ' со счётом ' + self.score)
+
+    def gen_english(self):
+        return self.wrap('The ' + self.winner + ' picked up a ' + self.score + ' win against the '
+                         + self.looser)
+
+
+EVENT_CLASSES = [WinnerEvent]
+
+
+def form_report(log):
+    russian_report = []
+    english_report = []
+    reports = [russian_report, english_report]
+    for event_class in EVENT_CLASSES:
+        event = event_class(log)
+        if event.is_applicable():
+            russian_report.append(event.gen_russian() + '. ')
+            english_report.append(event.gen_english() + '. ')
+    return [''.join(report) for report in reports]
+
+
 def main():
     logs = load_logs('Hockey_Log.xlsx')
-    reports = form_report(logs)
+    pprint(logs)
+    for log in logs:
+        for report in form_report(log):
+            print(report)
+
 
 if __name__ == '__main__':
     main()
