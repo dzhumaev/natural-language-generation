ff4888b7aa7577e2b28314f43f39ffb62f477238
dzhumaev <almir.dzhumaev@gmail.com>
2015-03-08 04:30:17 +0300

    Added overtime/shootout analysis

--- generate.py
+++ generate.py
@@ -5,19 +5,15 @@ from pprint import pprint
 
 import openpyxl
 
-
 def make_player(ws, row_index):
     i = str(row_index)
     last_name_ru, first_name_ru = ws['E' + i].value.split()
     last_name_en, first_name_en = ws['C' + i].value.split()
     return {
-        'first_name_ru': first_name_ru,
-        'last_name_ru': last_name_ru,
-        'first_name_en': first_name_en,
-        'last_name_en': last_name_en,
-        'role_en': ws['D' + i].value,
+        'first_name': {'en': first_name_en, 'ru': first_name_ru},
+        'last_name': {'en': last_name_en, 'ru': last_name_ru},
+        'role': {'en': ws['D' + i].value, 'ru': ws['F' + i].value},
     }
-
 
 def load_logs(file_name):
     logs = []
@@ -26,11 +22,11 @@ def load_logs(file_name):
         log = {}
         logs.append(log)
         log['date'] = ws['A2'].value
-        log['arena'] = ws['B2'].value
-        log['city'] = ws['C2'].value
+        log['arena'] = {'en': ws['B2'].value, 'ru': ws['B3'].value}
+        log['city'] = {'en': ws['C2'].value, 'ru': ws['C3'].value}
         log['attendance'] = ws['D2'].value
-        log['home-team'] = ws['E2'].value
-        log['guest-team'] = ws['F2'].value
+        log['home-team'] = {'en': ws['E2'].value, 'ru': ws['E3'].value}
+        log['guest-team'] = {'en': ws['F2'].value, 'ru': ws['F3'].value}
         log['players'] = defaultdict(dict)
         home_team_last_row = 4
         for i in range(4, 31):
@@ -43,33 +39,55 @@ def load_logs(file_name):
                 guest_team_last_row = i - 2
                 break
         for i in range(5, home_team_last_row+1):
-            log['players'][log['home-team']][ws['B'+str(i)].value] = make_player(ws, i)
+            log['players'][log['home-team']['en']][ws['B'+str(i)].value] = make_player(ws, i)
         for i in range(home_team_last_row+3, guest_team_last_row+1):
-            log['players'][log['guest-team']][ws['B'+str(i)].value] = make_player(ws, i)
+            log['players'][log['guest-team']['en']][ws['B'+str(i)].value] = make_player(ws, i)
 
         row = ws.get_highest_row()
         log['score'] = [ws['B'+str(row-1)].value, ws['B'+str(row)].value]
         
         log['goals'] = []
+        log['assists'] = []
+        log['time'] = 'regulation'
         for i in range(guest_team_last_row+3, row-2):
             if ws['J'+str(i)].value in {'scored', 'powerplay', 'shorthanded'}:
                 team = ws['D'+str(i)].value
                 author = log['players'][team][ws['E'+str(i)].value]
-                log['goals'].append({
-                    'minute': ws['B'+str(i)].value,
-                    'team': team,
-                    'author': author,
-                    'type': ws['J'+str(i)].value,
-                })
+                log['winning_goal'] = author
+                if type(ws['B'+str(i)].value) == int:
+                    log['goals'].append({
+                        'minute': ws['B'+str(i)].value,
+                        'team': team,
+                        'author': author,
+                        'type': ws['J'+str(i)].value,
+                    })
+                    if type(ws['H'+str(i)].value) == int:
+                        log['assists'].append({
+                            'minute': ws['B'+str(i)].value,
+                            'team': team,
+                            'author': log['players'][team][ws['H'+str(i)].value],
+                        })
+                    if type(ws['I'+str(i)].value) == int:
+                        log['assists'].append({
+                            'minute': ws['B'+str(i)].value,
+                            'team': team,
+                            'author': log['players'][team][ws['I'+str(i)].value],
+                        })
+
+            if ws['A'+str(i)].value == 'End of overtime':
+                log['time'] = 'overtime'
+            if ws['A'+str(i)].value == 'Shootout':
+                log['time'] = 'shootout'
+                
         process_log(log)
     return logs
 
 
 def process_log(log):
     if log['score'][0] > log['score'][1]:
-        log['winner'], log['looser'] = log['home-team'], log['guest-team']
+        log['winner'], log['loser'] = log['home-team'], log['guest-team']
     else:
-        log['looser'], log['winner'] = log['home-team'], log['guest-team']
+        log['loser'], log['winner'] = log['home-team'], log['guest-team']
     log['score'] = '-'.join(map(str, sorted(log['score'], reverse=True)))
 
 
@@ -109,12 +127,16 @@ class HighPitchEvent(Event):
 
 class WinnerEvent(HighPitchEvent):
     def gen_russian(self):
-        return (self.log['winner'] + ' обыграл ' + self.log['looser'] + ' со счётом '
-                + self.log['score'])
+        time = {'regulation': ' в основное время', 'overtime': ' в овертайме',
+                'shootout': ' по буллитам'}
+        return (self.log['winner']['ru'] + ' обыграл ' + self.log['loser']['ru']
+                + time[self.log['time']] + ' со счётом ' + self.log['score'])
 
     def gen_english(self):
-        return ('The ' + self.log['winner'] + ' picked up a ' + self.log['score']
-                + ' win against the ' + self.log['looser'])
+        time = {'regulation': ' in regulation', 'overtime': ' in overtime',
+                'shootout': ' in shootout'}
+        return (self.log['winner']['en'] + ' picked up a ' + self.log['score']
+                + ' win against ' + self.log['loser']['en'] + time[self.log['time']])
 
 
 def join_with_and(words, and_word):
@@ -135,26 +157,35 @@ class GoalsSummaryEvent(HighPitchEvent):
         self.goals_by_team_ru = defaultdict(set)
         self.goals_by_team_en = defaultdict(set)
         for goal in log['goals']:
-            self.goals_by_team_ru[goal['team']].add(goal['author']['last_name_ru'])
-            self.goals_by_team_en[goal['team']].add(goal['author']['last_name_en'])
+            self.goals_by_team_ru[goal['team']].add(goal['author']['last_name']['ru'])
+            self.goals_by_team_en[goal['team']].add(goal['author']['last_name']['en'])
         for k, v in self.goals_by_team_ru.items():
             self.goals_by_team_ru[k] = sorted(v)
         for k, v in self.goals_by_team_en.items():
             self.goals_by_team_en[k] = sorted(v)
 
     def gen_russian(self):
-        return ('В составе ' + self.log['winner'] + ' отличились '
-                + join_with_and_ru(self.goals_by_team_ru[self.log['winner']]) + ', за ' + self.log['looser']
-                + ' голы забили ' + join_with_and_ru(self.goals_by_team_ru[self.log['looser']]))
+        winning = {'regulation': '', 'overtime': ', ',
+                  'shootout': ', ' + self.log['winning_goal']['last_name']['ru']
+                  + ' забросил победный буллит'}
+        return ('В составе ' + self.log['winner']['ru'] + ' отличились '
+                + join_with_and_ru(self.goals_by_team_ru[self.log['winner']['en']]) 
+                + winning[self.log['time']]
+                + ', а за ' + self.log['loser']['ru'] + ' шайбы забили '
+                + join_with_and_ru(self.goals_by_team_ru[self.log['loser']['en']]))
 
     def gen_english(self):
-        return (join_with_and_en(self.goals_by_team_en[self.log['winner']])
-                + ' were on target for the ' + self.log['winner']  + ', while '
-                + join_with_and_en(self.goals_by_team_en[self.log['looser']])
-                + ' replied for the ' + self.log['looser'])
+        winning = {'regulation': '', 'overtime': ', ',
+                  'shootout': ', ' + self.log['winning_goal']['last_name']['en']
+                  + ' scored the winning bullet'}
+        return (join_with_and_en(self.goals_by_team_en[self.log['winner']['en']])
+                + ' were on target for ' + self.log['winner']['en']
+                + winning[self.log['time']] + ', while '
+                + join_with_and_en(self.goals_by_team_en[self.log['loser']['en']])
+                + ' replied for ' + self.log['loser']['en'])
 
 
-EVENT_CLASSES = [WinnerEvent, GoalsSummaryEvent, Pause]
+EVENT_CLASSES = [WinnerEvent, GoalsSummaryEvent]#, Pause]
 
 
 def form_report(log):
@@ -171,7 +202,7 @@ def form_report(log):
 
 def main():
     logs = load_logs('Hockey_Log.xlsx')
-    pprint(logs)
+    # pprint(logs)
     for log in logs:
         for report in form_report(log):
             print(report)
